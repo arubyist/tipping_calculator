d9ddac71afac4ed005ae9b5f020fef18c69c838e
arubyist <teachystuff@yahoo.com>
2017-03-01 02:49:42 -0800

    format display of numbers

--- app/models/tippy.rb
+++ app/models/tippy.rb
@@ -3,8 +3,14 @@ class Tippy < ApplicationRecord
 	validates :tip, presence: true
 	validates :cost, presence: true
 
+	TIP_CHOICES = { "10%" => ".10", "20%" => ".20", "30%" => ".30" }
+
+
+	
 	def calculation_of_total_cost 
 		cost + (tip * cost)
 	end
+
+
 
 end
