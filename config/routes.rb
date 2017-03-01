c097def5ee6f0c1a13c2ec29cac36809dcce3422
arubyist <teachystuff@yahoo.com>
2017-03-01 00:53:45 -0800

    add bootstrap gem and simple css modifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
-  root :to => 'tippies#index'
+  #root :to => 'tippies#index'
+  root :to => 'tippies#new'
 
   resources :tippies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
