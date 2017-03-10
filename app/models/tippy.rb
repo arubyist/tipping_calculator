dbb17d503191ae862a6f2403213479ab3432d92e
arubyist <teachystuff@yahoo.com>
2017-03-09 20:27:50 -0800

    minor cosmetic change plus more percentage added

--- app/models/tippy.rb
+++ app/models/tippy.rb
@@ -3,7 +3,8 @@ class Tippy < ApplicationRecord
 	validates :tip, presence: true
 	validates :cost, presence: true
 
-	TIP_CHOICES = { "10%" => ".10", "20%" => ".20", "30%" => ".30" }
+	TIP_CHOICES = { "10%" => ".10", "20%" => ".20", "30%" => ".30", "40%" => ".40", "50%" => ".50", 
+	                "60%" => ".60", "70%" => ".70", "80%" => ".80", "90%" => ".90" }
 
 
 	
